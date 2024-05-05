957cd3e8eac4c63e1d156a8977066df66b3f58ce
Kraaven <dhruv.bhojaraj@gmail.com>
2024-05-05 06:29:04 +0530

    Revert "added timed echolocation"
    This reverts commit 00c3419e20da7236749c8d02414cdd3d70c5d377.

--- scripts/detection_ray.gd
+++ scripts/detection_ray.gd
@@ -1,33 +1,29 @@
 extends RayCast2D
 
 var EcoP = preload("res://Scenes/EchoParticle.tscn")
-@onready var timer = $Timer
-
 var P
 var D = 0.0
 var X
 var Yoffset = 0.0
 var show = false
-var world
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Yoffset = target_position.y
-	timer.start()
+	P = EcoP.instantiate()
+	add_child(P)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta):
-	pass
-
-
-func _on_timer_timeout():
+func _process(delta):
 	if(is_colliding()):
-		P = EcoP.instantiate()
-		world.add_child(P)
 		X = get_collision_point()
+		P.visible = true
 		P.global_position = X
+		
 		D = global_position.distance_to(X)
 		var C = Color(D/Yoffset,D/Yoffset,D/Yoffset,1.0)
+		if(show):
+			print(position)
 		P.modulate = C
-		P.initialize(D)
-	timer.start()
+	else:
+		P.visible = false
